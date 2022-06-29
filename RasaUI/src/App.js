cd56db08ee18fbc62c174eedceff06f57eca4cb2
alasali1 <alasali96@gmail.com>
2022-06-28 18:28:27 -0700

    added a GUI

---
+++ RasaUI/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Basic from "./components/Basic"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Shoe Store</h1>
+      <Basic />
+    </div>
+  );
+}
+
+export default App;
